ebb7e3f18d1bf0df9c544e3c286263471f070987
Noah Kim <kimn@ansto.gov.au>
2025-03-27 16:50:37 +1100

    build: modify typescript and vite configurations to publish to npm registry

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: "./index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+      include: ["src/**/*", "index.ts"],
+      outDir: "dist",
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
 });
